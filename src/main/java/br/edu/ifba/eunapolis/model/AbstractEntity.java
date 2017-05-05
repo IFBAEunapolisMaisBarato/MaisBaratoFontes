56f40df2ad7e9e26fc4cbc6fa615e8dfa499e3c5
Leandro Costa <leandro.costa@ifba.edu.br>
2017-05-05 17:16:11 -0300

    close #35

--- src/main/java/br/edu/ifba/eunapolis/model/AbstractEntity.java
+++ src/main/java/br/edu/ifba/eunapolis/model/AbstractEntity.java
@@ -2,8 +2,37 @@ package br.edu.ifba.eunapolis.model;
 
 import java.io.Serializable;
 
-public interface AbstractEntity extends Serializable {
+public abstract class AbstractEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public abstract Long getId();
 	
-	Long getId();
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+		return result;
+	}
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractEntity other = (AbstractEntity) obj;
+		if (getId() == null) {
+			if (other.getId() != null)
+				return false;
+		} else if (!getId().equals(other.getId()))
+			return false;
+		return true;
+	}
 
 }
