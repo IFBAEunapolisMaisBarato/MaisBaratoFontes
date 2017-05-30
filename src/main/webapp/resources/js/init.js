b36ce1ecd91b770ff24b0d062db986ce14cb477d
MichaelRocha <michaeldesigners@gmail.com>
2017-05-30 07:33:30 -0300

    Correções menu EListagem de MenorPreco

--- src/main/webapp/resources/js/init.js
+++ src/main/webapp/resources/js/init.js
@@ -1,7 +1,12 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+        menuWidth: 250, // Default is 300
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+      });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
